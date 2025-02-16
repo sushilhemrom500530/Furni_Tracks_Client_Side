25fa9d01ca6bcf5975f84c0d7c10402cd5c72434
Sushil Hemrom <sushilhemrom98@gmail.com>
2025-02-16 09:08:16 +0600

    add update create shop in user pannel

--- src/layouts/dashboard/user/create shop/DCreateShop.jsx
+++ src/layouts/dashboard/user/create shop/DCreateShop.jsx
@@ -6,6 +6,8 @@ import { useAuth } from "../../../../hooks/useAuth";
 import { useState } from "react";
 import { FaSpinner } from "react-icons/fa";
 import { MdAddShoppingCart, MdDeleteForever } from "react-icons/md";
+import AdminTitle from "../../../../components/title";
+import Button from "../../../../components/button";
 
 const DCreateShop = () => {
     const axiosSecure = useAxiosSecure();
@@ -70,12 +72,14 @@ const DCreateShop = () => {
         }
     }
     return (
-        <div className="mt-12">
+        <div className="my-8">
             <div className='lg:w-1/2 w-full bg-white text-black shadow mx-auto px-10 py-12 '>
                 {/* <Helmet>
             <title>Create Shop | Inventory M </title>
         </Helmet> */}
-                <h2 className="text-2xl lg:text-4xl font-bold text-center my-12 flex items-center justify-center gap-2">Create a New Shop   <span className="mr-3 text-5xl text-center text-indigo-500">{<MdAddShoppingCart />}</span></h2>
+                <AdminTitle className="flex items-center justify-center gap-2">
+                    Create Shop <span><MdAddShoppingCart size={26} color="black" /></span>
+                </AdminTitle>
                 <form onSubmit={handleCreateShop}>
                     <div className='grid grid-cols-1 '>
                         <div className='space-y-6'>
@@ -85,7 +89,7 @@ const DCreateShop = () => {
                                         Your Shop Name
                                     </label>
                                     <input
-                                        className='input input-bordered input-info w-full text-white px-4 py-3 border rounded-md border-blue-400 '
+                                        className='input input-bordered input-info w-full text-black bg-white px-4 py-3 border rounded-md border-blue-400 '
                                         name='shop_name' id='shop_name' type='text' placeholder='your shop name...' required
                                     />
                                 </div>
@@ -93,42 +97,39 @@ const DCreateShop = () => {
                                     <label htmlFor='location' className='block text-black font-medium'>
                                         Location
                                     </label>
-                                    <input className='w-full px-4 py-3 text-white border rounded-md input input-bordered input-info border-blue-400 '
+                                    <input className='w-full px-4 py-3  text-black bg-white border rounded-md input input-bordered input-info border-blue-400 '
                                         name='location' id='location' type='text' placeholder='Location...' required
                                     />
                                 </div>
                             </div>
-                            <div className=' p-4 bg-white w-full m-auto rounded-lg'>
+                            <div className=' bg-white w-full m-auto rounded-lg'>
                                 <label className="block text-black font-medium"> Shop Logo</label>
                                 <div className=' bg-white w-full m-auto rounded-lg mt-3'>
-                                    <div className='file_upload px-5 py-3 relative border-4 border-dotted border-gray-300 rounded-lg overflow-hidden'>
-                                        {
-                                            showImage ? <div className='relative'><img src={showImage} alt="imageShow" className='w-full h-44' /> <span onClick={handleRemoveImage} className='absolute rounded-full -top-3 -right-6 cursor-pointer'><MdDeleteForever className='text-5xl text-red-500' /></span></div> : <input onChange={handleImage} type='file' name='image' id='image' accept='image/*' className="file-input w-full file-input-info focus:border-none bg-transparent" placeholder='choose your image.....' />
-                                        }
-                                        {/* <input type='file' name='image' id='image' accept='image/*' className="file-input w-full file-input-info focus:border-none " /> */}
-                                    </div>
+                                    {
+                                        showImage ? <div className='relative'><img src={showImage} alt="imageShow" className='w-full h-44' /> <span onClick={handleRemoveImage} className='absolute rounded-full -top-3 -right-6 cursor-pointer'><MdDeleteForever className='text-5xl text-red-500' /></span></div> : <input onChange={handleImage} type='file' name='image' id='image' accept='image/*' className="file-input w-full file-input-info focus:border-none bg-transparent" placeholder='choose your image.....' />
+                                    }
                                 </div>
                             </div>
                             <div className='space-y-1 '>
                                 <label htmlFor='description' className='block font-medium'>
                                     Description
                                 </label>
-                                <textarea id='description' className='block focus:rose-300 w-full h-32 px-4 py-3 text-white  border rounded-md input input-bordered input-info border-blue-400 ' name='description' placeholder="Write description..."
+                                <textarea id='description' className='block focus:rose-300 w-full h-32 px-4 py-3  text-black bg-white border rounded-md input input-bordered input-info border-blue-400 ' name='description' placeholder="Write description..."
                                 ></textarea>
                             </div>
                         </div>
 
-                        <button type='submit' className='btn w-full p-3 mt-5 text-[18px] text-center font-medium hover:text-white transition duration-200 rounded shadow-md bg-gradient-to-r from-[#0939e8] to-[#ff0fdb] text-black ' >
+                        <Button disabled={loading} className="custom-bg-color py-2 px-4 hover-custom-bg-color !w-full mt-6">
                             {loading ? (
                                 <span className='flex items-center justify-center gap-3'> <FaSpinner className='m-auto animate-spin' size={24} /> Processing....</span>
                             ) : (
                                 'Create Shop'
                             )}
-                        </button>
+                        </Button>
                     </div>
                 </form>
             </div>
-        </div>
+        </div >
     );
 };
 
